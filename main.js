2ca6d965df82ae74554e5f7378d23527ce2de012
RaphaCarmo <144746717+RaphaCarmo@users.noreply.github.com>
2024-05-21 20:24:02 -0300

    Construção do Main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+let nomeUsuário = prompt("Qual o seu nome?");
+let elemento = document.querySelector("#nome-usuário");
+
+
+while(nomeUsuário == ""){
+    nomeUsuário = prompt("Qual o seu nome?");
+}
+
+if(nomeUsuário == null){
+    elemento.textContent = 'Seja Bem Vindo';
+}
+else{
+    elemento.textContent = nomeUsuário;
+}
